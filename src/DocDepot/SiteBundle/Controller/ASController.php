46a4d767d6a51f7cb815afa612a9998be71627c6
Frédéric GONTIER <fredgont@yahoo.fr>
2017-08-22 15:43:46 +0200

    implmentation de service de recherche et suppression des AS

--- src/DocDepot/SiteBundle/Controller/ASController.php
+++ src/DocDepot/SiteBundle/Controller/ASController.php
@@ -3,27 +3,65 @@
 namespace DocDepot\SiteBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
-
+use DocDepot\SiteBundle\Entity\ActeurSocialRepository;
 
 class ASController extends FOSRestController
 {
-    public function getAsAction($slug)
+
+    private $repository;
+
+    public function getRepository(){
+        if($this->repository == null){
+            $this->repository = $this-> getDoctrine()-> getManager()->getRepository("DocDepotSiteBundle:ActeurSocial");
+        }
+        return $this->repository ;
+    }
+
+
+    private function getDefaultView($data)
     {
-        $data = array('mtest' => array('name' => "fred", "id" => $slug ));; // get data, in this case list of users.
-        $view = $this->view($data, 200)
+        return $this->view($data, 200)
             ->setTemplate("MyBundle:Users:getUsers.html.twig")
             ->setTemplateVar('users')
         ;
-        return $this->handleView($view);
     }
-
-    public function redirectAction()
+    
+    public function getActeursocialsAction()
     {
-        $view = $this->redirectView($this->generateUrl('some_route'), 301);
-        // or
-        $view = $this->routeRedirectView('some_route', array(), 301);
-
+        $as = $this->getRepository()->findAll();
+        $view = $this->getDefaultView($as);   
         return $this->handleView($view);
-    }
+    } // "get_user_beneficiaires"   [GET] /acteursocials
+
+    public function getActeursocialAction($idAs)
+    {
+        $as = $this->getRepository()->findOneById($idAs);
+        $view = $this->getDefaultView($as);   
+        return $this->handleView($view);
+    } // "get_user_beneficiaire"    [GET] /acteursocials/{idAs}
+
+    public function deleteActeursocialAction($idAs)
+    {
+        $as = $this->getRepository()->remove($this->getRepository()->findOneById($idAs));
+
+    } // "delete_user_beneficiaire" [DELETE] /acteursocials/{idAs}
+
+    public function newActeursocialAction($idAs)
+    {} // "new_user_beneficiaires"   [GET] /acteursocials/{idAs}/new
+
+    public function editActeursocialAction($idAs, $idBenef)
+    {} // "edit_user_beneficiaire"   [GET] /acteursocials/{idAs}/edit
+
+    public function removeActeursocialAction($idAs)
+    {
+        $this-> getDoctrine()-> getManager()->beginTransaction();
+        $entity = $this->getRepository()->findOneById($idAs);
+        $as = $this-> getDoctrine()-> getManager()->remove($entity);
+        $this-> getDoctrine()-> getManager()->commit();
+        $view = $this->getDefaultView(1);   
+        return $this->handleView($view);
+    } // "remove_user_beneficiaire" [GET] /acteursocials/{idAs}/remove
+
+
 }
 ?>
